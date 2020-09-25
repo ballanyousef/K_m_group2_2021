8ec5b5af0c49660fd2c97daceb181b70645a276a
Yousef Ballan <ballan_yousef@yahoo.com>
2020-09-25 11:39:25 +0300

    Initial commit

---
+++ app/src/main/java/yb/it/k_m_group2_2021/MainActivity.java
@@ -0,0 +1,14 @@
+package yb.it.k_m_group2_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
